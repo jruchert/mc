e66cad25b9b8bb270aa42401c8748b2ec00aa13f
coding <coding@coding.net>
2019-01-16 07:47:41 +0000

    MGC - Some function changes to my ch2_ex2 and some "pretty" changes as well.

--- ch02_ex2/display_results.php
+++ ch02_ex2/display_results.php
@@ -1,71 +1,74 @@
 <?php
-    // get the data from the form
-    $investment = filter_input(INPUT_POST, 'investment',
-        FILTER_VALIDATE_FLOAT);
-    $interest_rate = filter_input(INPUT_POST, 'interest_rate',
-        FILTER_VALIDATE_FLOAT);
-    $years = filter_input(INPUT_POST, 'years',
-        FILTER_VALIDATE_INT);
-
-    // validate investment
-    if ($investment === FALSE ) {
-        $error_message = 'Investment must be a valid number.'; 
-    } else if ( $investment <= 0 ) {
-        $error_message = 'Investment must be greater than zero.'; 
+// get the data from the form
+$investment = filter_input(INPUT_POST, 'investment',
+    FILTER_VALIDATE_FLOAT);
+$interest_rate = filter_input(INPUT_POST, 'interest_rate',
+    FILTER_VALIDATE_FLOAT);
+$years = filter_input(INPUT_POST, 'years',
+    FILTER_VALIDATE_INT);
+// validate investment
+$error_message = '';
+if (false === $investment) {
+    $error_message = 'Investment must be a valid number.';
+}
+if (0 > $investment) {
+    $error_message = 'Investment must be greater than zero.';
     // validate interest rate
-    } else if ( $interest_rate === FALSE )  {
-        $error_message = 'Interest rate must be a valid number.'; 
-    } else if ( $interest_rate <= 0 ) {
-        $error_message = 'Interest rate must be greater than zero.'; 
-    // validate years
-    } else if ( $years === FALSE ) {
-        $error_message = 'Years must be a valid whole number.';
-    } else if ( $years <= 0 ) {
-        $error_message = 'Years must be greater than zero.';
-    } else if ( $years > 30 ) {
-        $error_message = 'Years must be less than 31.';
-    // set error message to empty string if no invalid entries
-    } else {
-        $error_message = ''; 
-    }
-
-    // if an error message exists, go to the index page
-    if ($error_message != '') {
-        include('index.php');
-        exit(); }
-
-    // calculate the future value
-    $future_value = $investment;
-    for ($i = 1; $i <= $years; $i++) {
-        $future_value += $future_value * $interest_rate * .01; 
-    }
-
-    // apply currency and percent formatting
-    $investment_f = '$'.number_format($investment, 2);
-    $yearly_rate_f = $interest_rate.'%';
-    $future_value_f = '$'.number_format($future_value, 2);
+}
+if (false === $interest_rate) {
+    $error_message = 'Interest rate must be a valid number.';
+}
+if (0.01 > $interest_rate || 15 < $interest_rate) {
+    $error_message = 'Interest rate must be between 0.01 and 15%.';
+}
+if (false === $years) {
+    $error_message = 'Years must be a valid whole number.';
+}
+if (0 > $years || 31 < $years) {
+    $error_message = 'Years must be between 1 and 31.';
+}
+// if an error message exists, go to the index page
+if ('' !== $error_message) {
+    require 'index.php';
+    exit();
+}
+// calculate the future value
+$future_value = $investment;
+for ($i = 1; $i <= $years; $i++) {
+    $future_value += $future_value * $interest_rate * .01;
+}
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title>Future Value Calculator</title>
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
-    <main>
-        <h1>Future Value Calculator</h1>
-
-        <label>Investment Amount:</label>
-        <span><?php echo $investment_f; ?></span><br>
-
-        <label>Yearly Interest Rate:</label>
-        <span><?php echo $yearly_rate_f; ?></span><br>
-
-        <label>Number of Years:</label>
-        <span><?php echo $years; ?></span><br>
-
-        <label>Future Value:</label>
-        <span><?php echo $future_value_f; ?></span><br>
-    </main>
+<main>
+    <h1>Future Value Calculator</h1>
+    <form action="display_results.php" method="post">
+        <div id="data">
+            <label><span>Investment Amount $</span>
+                <input disabled class="right" type="text" name="investment"
+                       value="<?= number_format($investment, 2); ?>">
+            </label>
+            <label><span>Yearly Interest Rate:</span>
+                <input disabled class="right" type="text" name="interest_rate"
+                       value="<?= number_format($interest_rate, 2); ?>">&nbsp;%
+            </label>
+            <label><span>Number of Years:</span>
+                <input disabled class="right" type="text" name="years"
+                       value="<?= $years; ?>">
+            </label>
+            <label><span>Future Value $</span>
+                <input disabled class="right" type="text" name="investment"
+                       value="<?= number_format($future_value, 2); ?>">
+            </label>
+        </div>
+        <label><span>Calculated on</span> <?= date('Y-m-d H:i:s'); ?></label>
+        <label><span>&nbsp;</span><button type="submit" formaction="index.php">&lt;&lt; Back</button></label>
+    </form>
+</main>
 </body>
 </html>
--- ch02_ex2/index.php
+++ ch02_ex2/index.php
@@ -1,47 +1,58 @@
-<?php 
-    //set default value of variables for initial page load
-    if (!isset($investment)) { $investment = ''; } 
-    if (!isset($interest_rate)) { $interest_rate = ''; } 
-    if (!isset($years)) { $years = ''; } 
-?> 
+<?php
+//set default value of variables for initial page load
+// See
+// http://us2.php.net/manual/en/language.operators.comparison.php#language.operators.comparison.ternary
+// for more info on the null coalescing operator.
+$investment = $investment ?? 1;
+//if (!isset($investment)) {
+//    $investment = '';
+//}
+$interest_rate = $interest_rate ?? 1;
+$years = $years ?? 1;
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title>Future Value Calculator</title>
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
-
 <body>
-    <main>
+<main>
     <h1>Future Value Calculator</h1>
-    <?php if (!empty($error_message)) { ?>
-        <p class="error"><?php echo htmlspecialchars($error_message); ?></p>
-    <?php } ?>
-    <form action="display_results.php" method="post">
-
+    <?php if (!empty($error_message)): ?>
+        <p class="error"><?= $error_message; ?></p>
+    <?php endif ?>
+    <form action="display_results.php" method="post" autocomplete="on">
         <div id="data">
-            <label>Investment Amount:</label>
-            <input type="text" name="investment"
-                   value="<?php echo htmlspecialchars($investment); ?>">
-            <br>
-
-            <label>Yearly Interest Rate:</label>
-            <input type="text" name="interest_rate"
-                   value="<?php echo htmlspecialchars($interest_rate); ?>">
-            <br>
-
-            <label>Number of Years:</label>
-            <input type="text" name="years"
-                   value="<?php echo htmlspecialchars($years); ?>">
-            <br>
+            <label><span>Investment Amount $</span>
+                <!--
+                Don't need to do extra escaping here because value can only
+                be given default or come from already validated user input in
+                display_results.php.
+                -->
+                <input class="right" required type="number" name="investment"
+                       min="0.01" step="0.01" autofocus
+                       value="<?= $investment; ?>">
+                <!--                <input type="text" name="investment"-->
+                <!--                       value="--><?php //echo htmlspecialchars($investment); ?><!--">-->
+            </label>
+            <label><span>Yearly Interest Rate:</span>
+                <input class="right" required type="number" name="interest_rate"
+                       min="0.01" max="15" step="0.01"
+                       value="<?= $interest_rate; ?>">&nbsp;%
+            </label>
+            <label><span>Number of Years:</span>
+                <input class="right" required type="number" name="years"
+                       min="1" max="31"
+                       value="<?= $years; ?>">
+            </label>
         </div>
-
         <div id="buttons">
-            <label>&nbsp;</label>
-            <input type="submit" value="Calculate"><br>
+            <label><span>&nbsp;</span>
+                <input type="submit" value="Calculate">
+            </label>
         </div>
-
     </form>
-    </main>
+</main>
 </body>
 </html>
